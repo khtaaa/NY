270aadadfd697b3a55c3fca7a32283055b87609e
khtaaa <kahata1128@gmail.com>
2017-10-04 15:21:24 +0900

    lineの調整

--- Assets/script/stage/nazoru.cs
+++ Assets/script/stage/nazoru.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class nazoru : MonoBehaviour {
 	public GameObject[] line;
+	public GameObject[] damy;
 	public int lineN;
 	public float lineL=0.2f;
 	public float lineW = 0.1f;
@@ -17,38 +18,44 @@ public class nazoru : MonoBehaviour {
 	void Update () {
 		
 		//lineの種類変更
-		if (Input.GetMouseButtonDown (1)) 
-		{
+		if (Input.GetMouseButtonDown (1)) {
 			lineN++;
 			if (lineN >= line.Length)
 				lineN = 0;
 		}
 
-		if(blockbar.RA>0)
-		{
-		//lineのスタート地点
-		if (Input.GetMouseButtonDown (0))
-		{
-			Tpos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-			Tpos.z = 0;
-		}
-
-		//lineをひく
-		if (Input.GetMouseButton (0))
-		{
-			Vector3 Spos = Tpos;
-			Vector3 Epos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-			Epos.z = 0;
-
-			if ((Epos - Spos).magnitude > lineL) {
-				GameObject obj = Instantiate (line [lineN], transform.position, transform.rotation) as GameObject;
-				obj.transform.position = (Spos + Epos) / 2;
-				obj.transform.right = (Epos - Spos).normalized;
-				obj.transform.localScale = new Vector3 ((Epos - Spos).magnitude, lineW, lineW);
-				obj.transform.parent = this.transform;
-				Tpos = Epos;
+		if (blockbar.RA > 0) {
+			if (Input.GetMouseButtonDown (0)) {
+				Tpos = Input.mousePosition;	
+				Tpos = Camera.main.ScreenToWorldPoint (Tpos);
+				Tpos.z = 0;
 			}
-		}
+
+
+			if (Input.GetMouseButton (0)) {
+			
+				Vector3 Spos = Tpos;
+				Vector3 Epos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				Epos.z = 0;
+
+				if ((Epos - Spos).magnitude > lineL) {
+					//RaycastHit2D hit = Physics2D.BoxCast (Tpos,new Vector2(), Vector3.forward,Mathf.Infinity,LayerMask.GetMask("enemy","player","floor"));
+					RaycastHit2D[] hit = new RaycastHit2D[5]; // Physics2D.Raycast (Tpos, Vector3.forward,Mathf.Infinity,LayerMask.GetMask("enemy","player","floor"));
+					GameObject obj = Instantiate (line [lineN], (Spos + Epos) / 2 + Vector3.back * 5f, transform.rotation) as GameObject;
+					obj.transform.right = (Epos - Spos).normalized;
+					obj.transform.localScale = new Vector3 ((Epos - Spos).magnitude, lineW, lineW);
+					obj.transform.parent = this.transform;
+					if (obj.GetComponent<BoxCollider2D> ().Cast (Vector2.zero, hit, 0f) != 0) {
+						Destroy (obj);
+						obj = Instantiate (damy [lineN], (Spos + Epos) / 2 + Vector3.back * 5f, transform.rotation) as GameObject;
+						obj.transform.right = (Epos - Spos).normalized;
+						obj.transform.localScale = new Vector3 ((Epos - Spos).magnitude, lineW, lineW);
+						obj.transform.parent = this.transform;
+					}
+
+					Tpos = Epos;
+				}
+			}
 		}
 	}
 }
