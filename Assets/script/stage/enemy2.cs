5ca1c38d3622ac7944f2e81c9f5792b320ed13b0
khtaaa <kahata1128@gmail.com>
2017-10-16 16:11:14 +0900

    敵を追加

---
+++ Assets/script/stage/enemy2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy2 : MonoBehaviour {
+	float attacktime=0;
+	public float maxtime=600;
+	public GameObject attackobj;
+	GameObject player;
+	public float speed=5f;
+
+	// Use this for initialization
+	void Start () {
+		attacktime = 0;
+		player = GameObject.Find ("player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 v2;
+		v2 = new Vector2 (player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y);
+
+		if(Vector2.Distance(player.transform.position,transform.position)<10){
+		attacktime += Time.deltaTime;
+		if (attacktime > maxtime) {
+			attacktime = 0;
+			GameObject obj = Instantiate (attackobj) as GameObject;
+			obj.transform.position = transform.position;
+			obj.GetComponent<Rigidbody2D> ().velocity = v2.normalized*speed;
+		}
+		}
+	}
+}
